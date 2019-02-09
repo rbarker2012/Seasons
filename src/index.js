11038d90e0646c922d470d8692f0bdae002a7e45
rbarker2012 <rbarker2012@gmail.com>
2019-02-09 14:00:01 -0700

    added state value to position

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
     constructor(props) {
         super(props);
-    }
 
+        this.state = { lat: null };
 
-
-    render() {
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                this.setState({  lat: position.coords.latitude }) ;
+            },
             err => console.log(err)
           );
+    }
 
-        return <div>Latitude: </div>
+    render() {
+            return <div>Latitude: {this.state.lat} </div>
     }
 }
 
